78c79de170e55b085b38418f1ce4c56a4b12c758
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-08-18 18:00:49 +0100

    input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import MyButton from './components/MyButton';
 import { useState } from "react";
 
 export default function App() {
-  const [visible, setVisible] = useState(true); // couleur initiale = noir
+  const [saisie, setSaisie] = useState("");   // valeur de l'input
+  const [message, setMessage] = useState(""); // message validé
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-    {visible && <p>Welcome to the React documentation! This page will give you an introduction to 80% of the React concepts that you will use on a daily basis. </p> }
-    <button onClick={()=> setVisible(!visible)}>{visible ? "Masquer" : "Afficher"}</button>
+      {/* champ de saisie */}
+      <input
+        type="text"
+        placeholder="Écris ton message"
+        value={saisie}
+        onChange={(e) => setSaisie(e.target.value)}
+      />
+
+      {/* bouton valider */}
+      <button onClick={() => setMessage(saisie)}>Valider</button>
+
+      {/* affichage uniquement après validation */}
+      {message && <p>Tu as écrit : {message}</p>}
     </div>
   );
 }
