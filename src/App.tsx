0c3fd0b565439fb820c4e0eb7cedd9333f1206e9
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-08-18 17:23:50 +0100

    afficher et masquer le texte

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import MyButton from './components/MyButton';
 import { useState } from "react";
 
 export default function App() {
-  const [color, setColor] = useState("black"); // couleur initiale = noir
+  const [visible, setVisible] = useState(true); // couleur initiale = noir
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1 style={{ color: color }}>Bonjour React 👋</h1>
-      <button onClick={() => setColor("red")}>Rouge</button>
-      <button onClick={() => setColor("blue")}>Bleu</button>
-      <button onClick={() => setColor("green")}>Vert</button>
-      <button onClick={() => setColor("black")}>Réinitialiser</button>
+    {visible && <p>Welcome to the React documentation! This page will give you an introduction to 80% of the React concepts that you will use on a daily basis. </p> }
+    <button onClick={()=> setVisible(!visible)}>{visible ? "Masquer" : "Afficher"}</button>
     </div>
   );
 }
