eddf19c01efce1859a67803e5285b73a91c36f4b
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-08-20 00:22:54 +0100

    compteur

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import { useState } from "react";
 
 export default function App() {
-  const [season, setSeason] = useState("https://static.vecteezy.com/ti/vecteur-libre/p1/4913873-cercle-icones-avec-arbre-pour-quatre-saisons-concept-vector-graphic-design-illustration-of-change-time-through-the-year-wild-nature-environment-for-camping-ski-vectoriel.jpg");
+  const [i, setCount] = useState(0);
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <img 
-        src = {season}        
-        alt="img" 
-        style={{ width: "300px", borderRadius: "10px" }}
-      />
+      <h1>Counter</h1>
       <div style={{ margin: "20px 0" }}>
-      <button onClick={()=>setSeason("https://www.equilibre-et-sens.fr/wp-content/uploads/2019/10/automne-comment-mieux-vivre-saison-01.jpg") }>Automne</button>
-      <button onClick={()=>setSeason("https://img.le-dictionnaire.com/printemps-saison.jpg") }>Printemps</button>
-      <button onClick={()=>setSeason("https://cdn.futura-sciences.com/sources/images/screen/EVENEMENT/Hiver/081116-hiver03-43.jpg") }>Hiver</button>
-      <button onClick={()=>setSeason("https://lokaterre.com/wp-content/uploads/2023/03/vacances-dete.jpg") }>Ete</button>
+      <button onClick={() => setCount(i + 1)} >click</button>
+      <p>tu as clicker {i}</p>
     </div>
     
     </div>
