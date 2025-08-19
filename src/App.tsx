7b66b2908d8e841b9432c558e5696b2f1f18f103
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-08-20 00:30:00 +0100

    compteur

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 
 export default function App() {
-  const [i, setCount] = useState(0);
+  const [i, setCount] = useState("gray ");
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Counter</h1>
+    <div style={{ textAlign: "center", marginTop: "50px", backgroundColor: i, height: "100vh" }}>
+      <h1>Fond de la fenetre </h1>
       <div style={{ margin: "20px 0" }}>
-      <button onClick={() => setCount(i + 1)} >click</button>
-      <p>tu as clicker {i}</p>
+      <button onClick={()=> setCount("white")}>Blan</button>
+      <button onClick={()=> setCount("red")}>Rouge</button>
+      <button onClick={()=> setCount("blue")}>Bleu</button>
+      <button onClick={()=> setCount("green")}>Vert</button>
+      <button onClick={()=> setCount("bleu")}>Reinitialisation</button>
     </div>
     
     </div>
