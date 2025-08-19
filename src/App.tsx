7f2af0bad6e84b8e040db9988bd5472f7c109ca4
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-08-20 00:12:41 +0100

    changer les images des 4 saisons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
 
 export default function App() {
-  const [saisie, setSaisie] = useState("");   // valeur de l'input
-  const [message, setMessage] = useState(""); // message validé
-
+  const [season, setSeason] = useState("https://static.vecteezy.com/ti/vecteur-libre/p1/4913873-cercle-icones-avec-arbre-pour-quatre-saisons-concept-vector-graphic-design-illustration-of-change-time-through-the-year-wild-nature-environment-for-camping-ski-vectoriel.jpg");
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      {/* champ de saisie */}
-      <input
-        type="text"
-        placeholder="Écris ton message"
-        value={saisie}
-        onChange={(e) => setSaisie(e.target.value)}
+      <img 
+        src = {season}        
+        alt="img" 
+        style={{ width: "300px", borderRadius: "10px" }}
       />
-
-      {/* bouton valider */}
-      <button onClick={() => setMessage(saisie)}>Valider</button>
-
-      {/* affichage uniquement après validation */}
-      {message && <p>Tu as écrit : {message}</p>}
+      <div style={{ margin: "20px 0" }}>
+      <button onClick={()=>setSeason("https://www.equilibre-et-sens.fr/wp-content/uploads/2019/10/automne-comment-mieux-vivre-saison-01.jpg") }>Automne</button>
+      <button onClick={()=>setSeason("https://img.le-dictionnaire.com/printemps-saison.jpg") }>Printemps</button>
+      <button onClick={()=>setSeason("https://cdn.futura-sciences.com/sources/images/screen/EVENEMENT/Hiver/081116-hiver03-43.jpg") }>Hiver</button>
+      <button onClick={()=>setSeason("https://lokaterre.com/wp-content/uploads/2023/03/vacances-dete.jpg") }>Ete</button>
+    </div>
+    
     </div>
   );
 }
