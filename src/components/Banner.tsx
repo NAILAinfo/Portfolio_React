c3049bd96b83fc9a11fe371ae6327924b678974a
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-09-13 16:11:37 +0100

    modification

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { Col, Container, Row , Button } from "react-bootstrap";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowDownCircle } from 'react-bootstrap-icons';  // ⬅️ nouveau import
+
 const Banner = () => {
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span> Hello, I'm </span>
-                        <h1>{"Naila Dahmani" } 
-                            <span className="wrap">Web devolopper </span>
-                        </h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                        <span>Hello, I'm </span>
+                        <h1>{"Naila Dahmani"} </h1>                            
+                        <span className="wrap">Web developer</span>
+                        <p>Innovative and proactive Front-end Developer, capable of developing websites and applications with elegant and innovative concepts for demanding clients. Talented in project management, team leadership and independent problem solving. Versatile and highly organized with expertise in project planning, concept improvement and programming verification.</p>
 
+                        <button onClick={() => console.log('connect')}>
+                          Let’s Connect <ArrowDownCircle size={25} />
+                        </button>
                     </Col>
                 </Row>
             </Container>
