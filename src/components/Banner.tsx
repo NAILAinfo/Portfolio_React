36029f686ec90ed4ec1ad352f4c0f713e7d0442b
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-09-13 17:31:30 +0100

    add links

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { ArrowDownCircle } from 'react-bootstrap-icons';  // ⬅️ nouveau import
+import { Col, Container, Row ,Button } from "react-bootstrap";
+import { ArrowDown ,Github, Linkedin , EnvelopeFill} from 'react-bootstrap-icons'; 
 
 const Banner = () => {
     return (
@@ -8,14 +8,53 @@ const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
+
                         <span>Hello, I'm </span>
                         <h1>{"Naila Dahmani"} </h1>                            
                         <span className="wrap">Web developer</span>
-                        <p>Innovative and proactive Front-end Developer, capable of developing websites and applications with elegant and innovative concepts for demanding clients. Talented in project management, team leadership and independent problem solving. Versatile and highly organized with expertise in project planning, concept improvement and programming verification.</p>
+                        <p>Innovative and proactive Front-end Developer, capable of developing websites and 
+                            applications with elegant and innovative concepts for demanding clients. Talented in 
+                            project management, team leadership and independent problem solving. Versatile and 
+                            highly organized with expertise in project planning, concept improvement and 
+                            programming verification.
+                        </p>
 
-                        <button onClick={() => console.log('connect')}>
-                          Let’s Connect <ArrowDownCircle size={25} />
-                        </button>
+                        <div className="social-links" style={{ marginBottom: "15px" }}>
+                                <a
+                                    href="https://github.com/ton-profil"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    style={{ marginRight: "20px" }}
+                                >
+                                    <Github size={40} />
+                                </a>
+
+                                <a
+                                    href="https://linkedin.com/in/ton-profil"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <Linkedin size={40} />
+                                </a>
+
+                                <Button
+                                    variant="primary"
+                                    href="https://docs.google.com/document/d/1uwaV6upgWd0D1TPsTzBUFdsf-IFabqsxwGyUnjmDYrY/edit?tab=t.0"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    style={{ marginLeft: "20px" , padding: "8px 15px" }}
+                                >
+                                    Resume
+                                </Button>
+
+                                </div>
+
+                            <button onClick={() => console.log('connect')}>
+                            Let’s Connect <ArrowDown size={20} />
+                            </button>
+
+                    </Col>
+                    <Col xs={12} md={6} xl={5} >
                     </Col>
                 </Row>
             </Container>
