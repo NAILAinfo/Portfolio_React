304c507a0d7b3949837b0f2f8a2aa96085ebe4b2
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-11-06 22:20:36 +0100

    installer  lucide-react et travailler sur l'affichage des certificats

--- src/components/Certifications/Certification.tsx
+++ src/components/Certifications/Certification.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
 const Certification = () => {
+    const certifications = [
+        {
+            title : "SQL (Intermediate)",
+            issuer: "HackerRank",
+            date: "2025",
+            skills: ["Data base", "SQL"],
+            verifyLink : "https://www.hackerrank.com/certificates/C374D48E098B"
+
+        }
+    ];
     return (
         <section id="Certifications">
             <h1 id="title">Certification</h1>
-            <div>
+            <p>Professional certifications that validate my expertise and commitment to continuous learning</p>
+            
+            <div id="certification-list">
                 
+                {certifications.map((cert, index) => (
+                    <div key={index} className="certification-card">
+                        <h2>{cert.title}</h2>
+                        <p>{cert.issuer}</p>
+                        <p>Issued: {cert.date}</p>
+                        <p>{cert.skills.join(", ")}</p>        
+
+                </div> ),
+           
+                )}
             </div>
         </section>
     );
